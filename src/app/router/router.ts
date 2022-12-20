27ff224e81026bfd3ea80553b403f9d068f0f577
KirillDemyanenko <demyanenkokg@gmail.com>
2022-12-20 20:58:53 +0300

    feat: add product route

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,3 +1,6 @@
+import { Product } from '../types'
+import { Loader } from '../loader/loader'
+
 export class Router {
   private readonly root: string
 
@@ -5,17 +8,26 @@ export class Router {
     this.root = root
   }
 
-  start (): string {
+  async start (loader: Loader): Promise<void> {
     const currentPage = window.location.href
+    console.log(window.location.origin.concat('/?product='))
+    if (currentPage.includes(window.location.origin.concat('/?product='))) {
+      const str = window.location.href
+      const productNumber = parseInt(str.replace(window.location.origin.concat('/?product='), ''), 10)
+      const product: Product = await loader.getSingleProduct(productNumber)
+      console.log(product)
+      return
+    }
     console.log(currentPage)
     switch (currentPage) {
-      // add your rotes here
       case (window.location.origin.concat('/')):
-        return 'its root'
+        console.log('its root')
+        return
       case (this.root.concat('/home')):
-        return 'its ok'
+        console.log('its ok')
+        return
     }
     (document.querySelector('body') as HTMLElement).innerHTML = '<img src="https://repost.uz/storage/uploads/2-1642399910-nadira-post-material.jpeg" alt="404" height="100%">'
-    return '404'
+    console.log('404')
   }
 }
