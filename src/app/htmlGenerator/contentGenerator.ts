811e730f64c2ac6a578dc3be1b7f334f9bb7e824
KirillDemyanenko <demyanenkokg@gmail.com>
2022-12-21 21:33:53 +0300

    refactor: prettier

--- src/app/htmlGenerator/contentGenerator.ts
+++ src/app/htmlGenerator/contentGenerator.ts
@@ -7,8 +7,7 @@ export class ContentGenerator {
       if (!brands.includes(el.brand.toString())) {
         brands.push(el.brand.trim())
       }
-    }
-    )
+    })
     brands.sort()
     for (let i = 0; i < brands.length; i++) {
       const child = document.createElement('div') as HTMLElement
@@ -27,20 +26,25 @@ export class ContentGenerator {
       child.classList.add('main__category-item')
       child.classList.add('item-not-active')
       child.innerHTML = `<input type="checkbox" id="category${i.toString()}">
-                           <label for="category${i.toString()}">${categories[i].toString()}</label>
+                           <label for="category${i.toString()}">${categories[
+        i
+      ].toString()}</label>
                            <span>(${i.toString()}/${categories.length.toString()})</span>`
       element.append(child)
     }
   }
 
-  generateProductItems (products: Product[], element: HTMLElement): void { // create by me
+  generateProductItems (products: Product[], element: HTMLElement): void {
+    // create by me
     for (let i = 0; i < products.length; i++) {
       // основной div, который мы добавляем в контейер
       const child = document.createElement('div') as HTMLElement
       child.classList.add('main__item', 'big-item')
       child.id = `product${products[i].id}`
       child.onclick = () => {
-        window.location.href = window.location.origin.concat(`?product=${products[i].id}`)
+        window.location.href = window.location.origin.concat(
+          `?product=${products[i].id}`
+        )
       }
       // div с товаром
       const mainProductItem = document.createElement('div') as HTMLElement
@@ -142,26 +146,36 @@ export class ContentGenerator {
   }
 
   showSingleProduct (product: Product): void {
-    const element: HTMLElement = document.querySelector('.main__popup') as HTMLElement
-    const mainContainer = document.querySelector('.main__container') as HTMLElement
+    const element: HTMLElement = document.querySelector(
+      '.main__popup'
+    ) as HTMLElement
+    const mainContainer = document.querySelector(
+      '.main__container'
+    ) as HTMLElement
     element.style.display = 'flex' as string
-    mainContainer.style.display = 'none';
-    (element.firstChild?.nextSibling as HTMLElement).onclick = () => {
+    mainContainer.style.display = 'none'
+    ;(element.firstChild?.nextSibling as HTMLElement).onclick = () => {
       element.style.display = 'none'
       mainContainer.style.display = 'flex'
     }
     // TODO: тут позакидывать  из переменной product: Product в форму всплывающего окна
     console.log(product)
     // Меняем navigation
-    const navPopup = Array.from(document.querySelectorAll('.main__popup-navigation a'))
+    const navPopup = Array.from(
+      document.querySelectorAll('.main__popup-navigation a')
+    )
     navPopup[1].innerHTML = product.category.toUpperCase()
     navPopup[2].innerHTML = product.brand.toUpperCase()
     navPopup[3].innerHTML = product.title.toUpperCase()
     // Меняем title окна товара
-    const titleProduct = document.querySelector('.main__popup-detail h3') as HTMLElement
+    const titleProduct = document.querySelector(
+      '.main__popup-detail h3'
+    ) as HTMLElement
     titleProduct.innerHTML = product.title
     // Меняем данные в описании товара
-    const productDetails = Array.from(document.querySelectorAll('.product__detail p'))
+    const productDetails = Array.from(
+      document.querySelectorAll('.product__detail p')
+    )
     productDetails[0].innerHTML = product.description
     productDetails[1].innerHTML = String(product.discountPercentage)
     productDetails[2].innerHTML = String(product.rating)
@@ -169,20 +183,26 @@ export class ContentGenerator {
     productDetails[4].innerHTML = product.brand
     productDetails[5].innerHTML = product.category
     // Меняем цену
-    const productPrice = document.querySelector('.product__price-btns span') as HTMLElement
+    const productPrice = document.querySelector(
+      '.product__price-btns span'
+    ) as HTMLElement
     productPrice.innerHTML = `€${product.price}`
     // Меняем изображение
     const productSlide = document.querySelector('.popup__slide') as HTMLElement
     productSlide.innerHTML = ''
     for (let i = 0; i < product.images.length; i++) {
       const productImg = document.createElement('img') as HTMLElement
-      (productImg as HTMLImageElement).src = `${product.images[i]}`
+      ;(productImg as HTMLImageElement).src = `${product.images[i]}`
       productSlide.append(productImg)
     }
-    const productBigImg = document.querySelector('.popup__big-photo img') as HTMLImageElement
+    const productBigImg = document.querySelector(
+      '.popup__big-photo img'
+    ) as HTMLImageElement
     productBigImg.src = `${product.thumbnail}`
     // Cмена изображения при клике
-    const sliderImgs = Array.from(document.querySelectorAll('.popup__slide img'))
+    const sliderImgs = Array.from(
+      document.querySelectorAll('.popup__slide img')
+    )
     sliderImgs.forEach((img) => {
       img.addEventListener('click', (): void => {
         productBigImg.src = `${(img as HTMLImageElement).src}`
