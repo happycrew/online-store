b25cdb45755293c5eb67a8b9a53c60d801cdc303
KirillDemyanenko <demyanenkokg@gmail.com>
2022-12-21 21:35:32 +0300

    refactor: prettier

--- src/app/loader/loader.ts
+++ src/app/loader/loader.ts
@@ -7,15 +7,23 @@ import { Product, ProductResponse } from '../types'
 
 export class Loader {
   async getCategorise (): Promise<string[]> {
-    return await (await (await fetch('https://dummyjson.com/products/categories')).json() as Promise<string[]>)
+    return await ((await (
+      await fetch('https://dummyjson.com/products/categories')
+    ).json()) as Promise<string[]>)
   }
 
   async getSingleProduct (elementId: number): Promise<Product> {
-    return await (await (await fetch('https://dummyjson.com/products/'.concat(elementId.toString()))).json() as Promise<Product>)
+    return await ((await (
+      await fetch(
+        'https://dummyjson.com/products/'.concat(elementId.toString())
+      )
+    ).json()) as Promise<Product>)
   }
 
   async getProducts (limit = 100, skip = 0): Promise<ProductResponse> {
     const url = `https://dummyjson.com/products?limit=${limit.toString()}`
-    return await (await (await fetch(skip === 0 ? url : url.concat(`&skip=${skip.toString()}`))).json() as Promise<ProductResponse>)
+    return await ((await (
+      await fetch(skip === 0 ? url : url.concat(`&skip=${skip.toString()}`))
+    ).json()) as Promise<ProductResponse>)
   }
 }
