f4c55eb26d4be86f8ce84683371b28642b814d37
KirillDemyanenko <demyanenkokg@gmail.com>
2022-12-21 21:32:10 +0300

    refactor: add product as parameter for router

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,24 +12,37 @@ export class App {
   private readonly productsBlock: HTMLElement // create by me
   private readonly clickChangeView: ClickChangeView // by me
 
-  constructor (loader: Loader, generator: ContentGenerator, changeview: ClickChangeView) {
+  constructor (
+    loader: Loader,
+    generator: ContentGenerator,
+    changeview: ClickChangeView
+  ) {
     this.loader = loader
     this.generator = generator
-    this.brandsBlock = document.querySelector('.main__brand-list') as HTMLElement
-    this.categoriesBlock = document.querySelector('.main__category-list') as HTMLElement
-    this.productsBlock = document.querySelector('.main__product-items') as HTMLElement // by me
+    this.brandsBlock = document.querySelector(
+      '.main__brand-list'
+    ) as HTMLElement
+    this.categoriesBlock = document.querySelector(
+      '.main__category-list'
+    ) as HTMLElement
+    this.productsBlock = document.querySelector(
+      '.main__product-items'
+    ) as HTMLElement // by me
     this.clickChangeView = changeview
   }
 
   async start (): Promise<void> {
     const router = new Router(window.location.origin.concat('/'))
-    await router.start(this.loader, this.generator).catch((err: Error) => {
-      throw new Error(err.message)
-    }).then().catch()
     const categories: string[] = (await this.loader.getCategorise()).sort()
     const products: ProductResponse = await this.loader.getProducts()
+    await router
+      .start(this.loader, this.generator, products.products)
+      .catch((err: Error) => {
+        throw new Error(err.message)
+      })
+      .then()
+      .catch()
     this.generator.generateBrandItems(products.products, this.brandsBlock)
     this.generator.generateCategoryItems(categories, this.categoriesBlock)
-    this.generator.generateProductItems(products.products, this.productsBlock)
   }
 }
