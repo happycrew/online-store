b0387661bf573266d7802243dbea702893ab3135
KirillDemyanenko <demyanenkokg@gmail.com>
2022-12-21 21:40:41 +0300

    feat: add sorting for products

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -4,31 +4,91 @@ import { ContentGenerator } from '../htmlGenerator/contentGenerator'
 
 export class Router {
   private readonly root: string
+  productsBlock = document.querySelector('.main__product-items') as HTMLElement
 
   constructor (root: string) {
     this.root = root
   }
 
-  async start (loader: Loader, generator: ContentGenerator): Promise<void> {
+  async start (
+    loader: Loader,
+    generator: ContentGenerator,
+    products: Product[]
+  ): Promise<void> {
     const currentPage = window.location.href
-    console.log(window.location.origin.concat('/?product='))
     if (currentPage.includes(window.location.origin.concat('/?product='))) {
       const str = window.location.href
-      const productNumber = parseInt(str.replace(window.location.origin.concat('/?product='), ''), 10)
+      const productNumber = parseInt(
+        str.replace(window.location.origin.concat('/?product='), ''),
+        10
+      )
       const product: Product = await loader.getSingleProduct(productNumber)
       generator.showSingleProduct(product)
       return
     }
-    console.log(currentPage)
+    if (currentPage.includes(this.root.concat('?sort='))) {
+      const sortingMethod = currentPage.replace(this.root.concat('?sort='), '')
+      switch (sortingMethod) {
+        case 'price-ASC':
+          generator.generateProductItems(
+            products.sort((a: Product, b: Product) =>
+              a.price > b.price ? 1 : -1
+            ),
+            this.productsBlock
+          )
+          return
+        case 'price-DESC':
+          generator.generateProductItems(
+            products.sort((a: Product, b: Product) =>
+              a.price < b.price ? 1 : -1
+            ),
+            this.productsBlock
+          )
+          return
+        case 'rating-ASC':
+          generator.generateProductItems(
+            products.sort((a: Product, b: Product) =>
+              a.rating > b.rating ? 1 : -1
+            ),
+            this.productsBlock
+          )
+          return
+        case 'rating-DESC':
+          generator.generateProductItems(
+            products.sort((a: Product, b: Product) =>
+              a.rating < b.rating ? 1 : -1
+            ),
+            this.productsBlock
+          )
+          return
+        case 'discount-ASC':
+          generator.generateProductItems(
+            products.sort((a: Product, b: Product) =>
+              a.discountPercentage > b.discountPercentage ? 1 : -1
+            ),
+            this.productsBlock
+          )
+          return
+        case 'discount-DESC':
+          generator.generateProductItems(
+            products.sort((a: Product, b: Product) =>
+              a.discountPercentage < b.discountPercentage ? 1 : -1
+            ),
+            this.productsBlock
+          )
+          return
+      }
+    }
     switch (currentPage) {
-      case (window.location.origin.concat('/')):
-        console.log('its root')
+      case this.root:
+        generator.generateProductItems(products, this.productsBlock)
         return
-      case (this.root.concat('/home')):
+      case this.root.concat('/home'):
         console.log('its ok')
         return
     }
-    (document.querySelector('body') as HTMLElement).innerHTML = '<img src="https://repost.uz/storage/uploads/2-1642399910-nadira-post-material.jpeg" alt="404" height="100%">'
+    (document.querySelector('body') as HTMLElement).innerHTML =
+      '<img src="https://repost.uz/storage/uploads/2-1642399910-nadira-post-material.jpeg" alt="404" height="100%">'
     console.log('404')
   }
 }
