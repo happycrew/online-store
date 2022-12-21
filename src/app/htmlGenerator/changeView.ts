ec2f85b65c6a09d0c34ee9a54967aa644c2bb642
KirillDemyanenko <demyanenkokg@gmail.com>
2022-12-21 21:33:10 +0300

    refactor: prettier

--- src/app/htmlGenerator/changeView.ts
+++ src/app/htmlGenerator/changeView.ts
@@ -6,22 +6,26 @@ export class ClickChangeView {
   }
 
   makeEvent (): void {
-    this.btns.forEach(btn => {
+    this.btns.forEach((btn) => {
       btn.addEventListener('click', this.changeView.bind(this))
     })
   }
 
   changeView (event: Event): void {
     if (!(event.target as HTMLElement).classList.contains('active-view')) {
-      const currentActive = document.querySelector('.active-view') as HTMLElement
-      currentActive.classList.remove('active-view');
-      (event.target as HTMLElement).classList.add('active-view')
+      const currentActive = document.querySelector(
+        '.active-view'
+      ) as HTMLElement
+      currentActive.classList.remove('active-view')
+      ;(event.target as HTMLElement).classList.add('active-view')
       if ((event.target as HTMLElement).classList.contains('view__small')) {
         const bigItemsArr = Array.from(document.querySelectorAll('.big-item'))
         const itemInfoArr = Array.from(document.querySelectorAll('.item__info'))
         itemInfoArr.forEach((el) => el.classList.toggle('info-hidden'))
         bigItemsArr.forEach((el) => el.classList.remove('big-item'))
-      } else if ((event.target as HTMLElement).classList.contains('view__big')) {
+      } else if (
+        (event.target as HTMLElement).classList.contains('view__big')
+      ) {
         const bigItemsArr = Array.from(document.querySelectorAll('.main__item'))
         bigItemsArr.forEach((el) => el.classList.add('big-item'))
         const itemInfoArr = Array.from(document.querySelectorAll('.item__info'))
