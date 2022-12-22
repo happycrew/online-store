d77f6e35690ae3c91d34bb976a631f7251ea4396
Vladimir Orlov <satancrew.true@gmail.com>
2022-12-22 17:36:09 +0300

    feat: add block total for main__cart container with styles

--- src/index.ts
+++ src/index.ts
@@ -95,7 +95,10 @@ selectSort.addEventListener('change', () => {
 })
 
 // Cкрыл все блоки кроме корзины
-const display1 = document.querySelector('.main__container') as HTMLElement
-display1.style.display = 'none'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const displayContainer = document.querySelector('.main__container') as HTMLElement
+// displayContainer.style.display = 'none'
 const cartEmpty = document.querySelector('.main__cart h1') as HTMLHeadingElement
 cartEmpty.style.display = 'none'
+const displayCart = document.querySelector('.main__cart') as HTMLElement
+displayCart.style.display = 'none'
