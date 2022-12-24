6d8c84c6499ecbb9dcf295ec0fdde3c556f0a4cc
Vladimir Orlov <satancrew.true@gmail.com>
2022-12-24 12:19:04 +0300

    feat: add some code about email validation

--- src/index.ts
+++ src/index.ts
@@ -109,11 +109,10 @@ displayModal.style.display = 'flex'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const phoneNumber = document.querySelector('.person-phone input') as HTMLInputElement
 const personName = document.querySelector('.person-name input') as HTMLInputElement
+const personAddress = document.querySelector('.person-adress input') as HTMLInputElement
 const personEmail = document.querySelector('.person-email input') as HTMLInputElement
 const validationBlocks = Array.from(document.querySelectorAll('.modal-form__personal-details div'))
-const validationInputs = Array.from(document.querySelectorAll('.modal-form__personal-details div input'))
-console.log(validationInputs[1])
-console.log(personName)
+
 function createError (block: HTMLElement): void {
   const divError = document.createElement('div')
   divError.classList.add('error')
@@ -140,9 +139,30 @@ function validateName (name: string): boolean {
   }
   return nameFlag
 }
+
 function validatePhone (phone: string | number): boolean {
   const re = /^[+][0-9]{11}$/
   return re.test(String(phone))
+}
+
+function validateAddress (address: string): boolean {
+  const checkAddress: string[] = address.split(' ')
+  let addressFlag = true
+  if (checkAddress.length < 3) {
+    return false
+  } else {
+    checkAddress.forEach((el) => {
+      if (el.length < 5) {
+        addressFlag = false
+      }
+    })
+  }
+  return addressFlag
+}
+
+function validateEmail (email: string): boolean {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase())
 }
 
 // Телефонный номер
@@ -160,7 +180,7 @@ phoneNumber.onchange = function (): void {
     }
   }
 }
-
+// Проверка имени
 personName.onchange = function (): void {
   const nameVal = personName.value
   if (!validateName(nameVal)) {
@@ -172,6 +192,21 @@ personName.onchange = function (): void {
     console.log('Display name correct')
     if (validationBlocks[0].children.length !== 1) {
       validationBlocks[0].removeChild(deleteError())
+    }
+  }
+}
+// Проверка адреса
+personAddress.onchange = function (): void {
+  const addressVal = personAddress.value
+  if (!validateAddress(addressVal)) {
+    console.log('Address incorrect')
+    if (validationBlocks[2].children.length === 1) {
+      createError(validationBlocks[2] as HTMLElement)
+    }
+  } else {
+    console.log('Address correct')
+    if (validationBlocks[2].children.length !== 1) {
+      validationBlocks[2].removeChild(deleteError())
     }
   }
 }
